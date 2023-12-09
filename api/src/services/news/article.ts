a0a7858b83e6486ba8975073bc54aad7fec812c5
siemah <hmi.dayenio@gmail.com>
2023-12-09 09:58:25 +0100

    feat:get article details from houston chronicle

--- api/src/services/news/article.ts
+++ api/src/services/news/article.ts
@@ -17,9 +17,12 @@ export async function getArticleContent({
   request: Request;
 }) {
   let article = {};
-  // from wire source
+  // from wire sources such as rockets/celtics wire
   if (provider.includes("wire") === true) {
     article = await getArticleFromWire(link, request);
+  } else if (/houston chronicle/i.test(provider) === true) {
+    // from houston chronicle
+    article = await getArticleFromChronicle(link, request);
   }
   // todo: from chronicle
   // todo: from onfootball
@@ -57,7 +60,41 @@ async function getArticleFromWire(url: string, req: Request) {
     date,
     author: {
       name: authorName,
-      link: authorLink
+      link: authorLink,
+      jobTitle: null
+    }
+  };
+}
+
+/**
+ * Get article body beside author details from houston chronicle
+ * 
+ * @param url webpage link
+ * @param req user request
+ * @returns article content with extra details about author
+ */
+async function getArticleFromChronicle(url: string, req: Request) {
+  const extraDataScraper = await initScraper(req, url);
+  // body + author + date
+  const authorBody = extraDataScraper.querySelector("[type='application/ld+json']");
+  const authorResponse = await authorBody.getText({ spaced: "" });
+  const [articleExtraData = "{}"] = authorResponse?.[authorBody?.selector];
+  const extraData = JSON.parse(articleExtraData);
+  const content = extraData?.articleBody || "N/A";
+  const description = extraData?.description || null;
+  const authorName = extraData?.author?.[0]?.name || "N/A";
+  const authorLink = extraData?.author?.[0]?.url || null;
+  const authorJobTitle = extraData?.author?.[0]?.jobTitle || null;
+  const date = extraData?.datePublished || null;
+
+  return {
+    content: content,
+    description,
+    date,
+    author: {
+      name: authorName,
+      link: authorLink,
+      jobTitle: authorJobTitle
     }
   };
 }
