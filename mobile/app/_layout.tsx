c2f7f9a4eb91fb1cb7955b8b5ecfecc741574ee9
siemah <hmi.dayenio@gmail.com>
2023-12-12 10:43:12 +0100

    conf:setup posthog for react native

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -4,6 +4,7 @@ import * as NavigationBar from 'expo-navigation-bar';
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
 import useScreenColor from '../hook/use-screen-color';
+import { PostHogProvider } from 'posthog-react-native'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,6 +14,10 @@ const queryClient = new QueryClient({
     }
   }
 });
+
+const postHogOptions = {
+  host: "https://eu.posthog.com",
+};
 
 export default function StackLayout() {
   const screenColor = useScreenColor();
@@ -29,7 +34,12 @@ export default function StackLayout() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack screenOptions={screenOptions} />
+      <PostHogProvider
+        apiKey="phc_RsmRdAMG6RSS4uM1F500S0BBvn1TC7gMxHbsqhg4wpj"
+        options={postHogOptions}
+      >
+        <Stack screenOptions={screenOptions} />
+      </PostHogProvider>
     </QueryClientProvider>
   );
 }
